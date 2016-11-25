7b58e3d9dbce1ff64c104ae84b7ad186520f0062
James Jing Tang <363541335@qq.com>
2016-11-25 21:28:02 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vim_conf ~/.vimrc
 cp bash_conf ~/.bashrc
-source ~/.bashrc
-echo "Open vim, then input :PluginInstall"
+echo "Input 'source ~/.bashrc' to activate bash configuration."
+echo "Open vim, then input :PluginInstall to install all vim plugins."
