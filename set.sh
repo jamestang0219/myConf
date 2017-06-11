47ba0bd8bcab1d36c7129bd10cd2770e5f9cc701
James Jing Tang <363541335@qq.com>
2017-06-11 20:21:14 +0800

    Create set.sh

--- set.sh
+++ set.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/seebi/dircolors-solarized.git ~/dircolors-solarized
+cp ~/dircolors-solarized/dircolors.ansi-dark ~/.dircolors
 cp vim_conf ~/.vimrc
 cp bash_conf ~/.bashrc
 echo "Input 'source ~/.bashrc' to activate bash configuration."
